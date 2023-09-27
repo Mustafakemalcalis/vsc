eeaaab0dcc636cef7b0673b9a31c04215f9abae4
Mustafakemalcalis <kkemal1202@gmail.com>
2023-09-27 10:09:33 +0300

    completed

--- src/Alan.java
+++ src/Alan.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Alan {
     public static void main(String[] args) {
-        int square,env;
-        double pi = 3
-    }
+        int square,env,radius;
+        int pi = 3;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Dairenin yarıçapını giriniz: ");
+
+        radius = scan.nextInt();
+        square = pi*radius*radius;
+        env = 2*pi*radius;
+
+        System.out.println("Dairenin alanı: " + square);
+        System.out.println("Dairenin çevresi: " + env);
+        }
 }
